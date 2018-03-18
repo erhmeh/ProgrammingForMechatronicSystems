afe21dbd1239fe5c8de752cf1695ffe11d5e8988
erhmeh <jaminearly@gmail.com>
2018-03-18 22:32:17 +1100

    Prework Week02
    Only did first two. Too tired to do the next one, will do in class.

---
+++ week02/classes.cpp
@@ -0,0 +1,107 @@
+/********************************************************************************
+ * Week 02 - Classes Prework
+ *
+ * The class "Rectangle" has three functions. These member functions are called
+ * setWidthHeight (accepts two int arguments and is a setter), area (does not accept
+ * any arguments and returns an int) and perimeter (does not accept any argmuments
+ * and returns an int). Access specifies are public and private and limit the
+ * access to data members. A setter method is required to set the rectangles width
+ * and height as these data members are private and cannot be modified elsewhere.
+ * It is best practice to keep classes in their own files however I have kept it
+ * together for ease of use.
+ *
+ *********************************************************************************/
+
+#include <iostream>
+#include <random>
+using namespace std;
+
+// Rectangle Class
+class Rectangle {
+public:
+void setWidthHeight (int,int);
+int area (void);
+int perimeter(void);
+
+private:
+int width_, height_;
+};
+
+// Random array Class
+class RandomArray {
+private:
+int seed_, size_;
+public:
+double randDoub_[];
+void setSeed(int);
+void setArraySize(int);
+void generate(void);
+};
+
+int Rectangle::area(void){
+        return (width_*height_);
+}
+
+int Rectangle::perimeter(void){
+        return (2*width_ + 2*height_);
+}
+
+void Rectangle::setWidthHeight(int i, int j){
+        width_ = i;
+        height_ = j;
+}
+
+void RandomArray::setSeed(int i){
+        seed_ = i;
+        srand(seed_); // Generate random seed
+}
+
+void RandomArray::setArraySize(int i){
+        size_ = i;
+        std::fill_n(randDoub_, size_, 0); // Clear array
+}
+
+void RandomArray::generate(void){
+        for (int i = 0; i < size_; i++) {
+                randDoub_[i] = (double)rand();
+        }
+}
+
+// Main
+int main() {
+        Rectangle squareBoi;
+        RandomArray randBoi;
+        int width;
+        int height;
+        int seed;
+        int arraySize;
+        while (true) {
+                char option;
+                std::cout << "Please choose the Class to demonstrate. For rectangle press 'r', for array 'a' or for sum of rectangles 's'" << std::endl;
+                cin >> option;
+                switch(option) {
+                case 'r':
+                        std::cout << "Please enter an integer value for height. " << std::endl;
+                        cin >> height;
+                        std::cout << "Please enter an integer value for width. " << std::endl;
+                        cin >> width;
+                        squareBoi.setWidthHeight(width, height);
+                        std::cout << "\n With a width of " << width << " and a height of " << height << " the area is " << squareBoi.area() << " and the perimeter is "<< squareBoi.perimeter() << " \n\n" << std::endl;
+                        break;
+                case 'a':
+                        std::cout << "Please enter an integer value for the seed. " << std::endl;
+                        cin >> seed;
+                        std::cout << "Please enter an integer value for the array size. " << std::endl;
+                        cin >> arraySize;
+                        randBoi.setSeed(seed);
+                        randBoi.setArraySize(arraySize);
+                        randBoi.generate();
+                        for(int i = 0; i < arraySize; i++) {
+                                std::cout << "[" << randBoi.randDoub_[i] << "] ";
+                        }
+                        std::cout << "\n" << std::endl
+                        break;
+                }
+        }
+        return 0;
+}
